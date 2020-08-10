e74f17272452ac70bf690b4907cf7321f9e44aa9
SrBenja007 <suprmeobenjamin.cia@gmail.com>
2020-08-10 01:19:01 -0400

    Update

--- src/main/java/team/cloudly/actions/FactoryActionImpl.java
+++ src/main/java/team/cloudly/actions/FactoryActionImpl.java
@@ -8,7 +8,13 @@ public class FactoryActionImpl implements FactoryAction {
     @Override
     public Action getAction(ActionType actionType, String identifier, ActionClass actionClass) {
         switch (actionType){
-            case MESSAGE:
+            case TITLE:
+
+            case ACTIONBAR:
+
+            case LOCATION:
+
+            default:
                 new ActionMessage(identifier);
         }
         return null;
